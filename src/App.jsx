8aca7cb8e14b16a2f1f5450eefb9195bce34613a
Errorpat-404 <nadgesachin644@gmail.com>
2023-11-15 16:21:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './components/navbar/navbar'
+import Footer from './components/footer/footer'
+import './App.css'
+import Login from './components/login/login';
+import Signup from './components/signup/signup';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Login />
+      {/* <Signup /> */}
+    </div>
+  )
+}
+
+export default App
